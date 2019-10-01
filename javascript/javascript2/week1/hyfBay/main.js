0e52af93cf5f27b30d3fd48fb8347365c48bcb91
MirSzabo <poradova.m@gmail.com>
2019-10-01 22:38:29 +0200

    Showing more details to the user

--- javascript/javascript2/week1/hyfBay/main.js
+++ javascript/javascript2/week1/hyfBay/main.js
@@ -4,11 +4,11 @@ const products = getAvailableProducts();
 console.log(products) // logs out
 
 const newUl = document.querySelector("section.products > ul");
-//Improving the renderProducts function
+//Showing more details to the user
 function renderProducts(products) {
   for (let i = 0; i < products.length; i++) {
     const newLi = document.createElement("li");
-    newLi.innerHTML = products[i].name;
+    newLi.innerHTML = `${products[i].name} | ${products[i].price} | ${products[i].rating} | ${products[i].shipsTo}`;
     newUl.appendChild(newLi);
     //console.log(newLi);
     }
