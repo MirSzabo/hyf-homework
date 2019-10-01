23cd79b5335aedd81d738322d5996421851be539
MirSzabo <poradova.m@gmail.com>
2019-10-02 01:12:43 +0200

    Shipped to multiple countries

--- javascript/javascript2/week1/hyfBay/main.js
+++ javascript/javascript2/week1/hyfBay/main.js
@@ -1,16 +1,44 @@
 console.log("Script loaded");
 
 const products = getAvailableProducts();
-console.log(products) // logs out
+console.log(products); // logs out
 
 const newUl = document.querySelector("section.products > ul");
-//Showing more details to the user
+//Optional ships to rendering.
+
 function renderProducts(products) {
-  for (let i = 0; i < products.length; i++) {
-    const newLi = document.createElement("li");
-    newLi.innerHTML = `${products[i].name} | ${products[i].price} | ${products[i].rating} | ${products[i].shipsTo}`;
-    newUl.appendChild(newLi);
-    //console.log(newLi);
+  const newLi = document.createElement("li");
+  newUl.appendChild(newLi);
+
+  //for each item create new nested ul
+  products.forEach(function(product) {
+    const newUlNested = document.createElement("ul");
+    newLi.appendChild(newUlNested);
+
+    //for each nested ul create nested li with classes
+    const keys = Object.keys(product);
+    //console.log(keys); //["id", "name", "price", "rating", "shipsTo"]
+
+    for (let i = 1; i < keys.length - 1; i++) {
+      const newLiNested = document.createElement("li");
+      newLiNested.innerHTML = product[keys[i]];
+      newLiNested.setAttribute("class", keys[i]);
+      newUlNested.appendChild(newLiNested);
     }
-};
-renderProducts(products); 
+
+    //for shipsTo create new nested ul with li of countries
+    const countryLi = document.createElement("li");
+    countryLi.setAttribute("class", "ships-to");
+    const newUlNested2 = document.createElement("ul");
+    countryLi.appendChild(newUlNested2);
+    newUlNested.appendChild(countryLi);
+
+    product.shipsTo.forEach(function(country) {
+      const newLiNested2 = document.createElement("li");
+      newLiNested2.innerHTML = country;
+      newUlNested2.appendChild(newLiNested2);
+    });
+  });
+}
+
+renderProducts(products);
