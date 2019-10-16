5d823b4f3308c6ed22db9407b47c263fd24fea4a
MirSzabo <poradova.m@gmail.com>
2019-10-16 13:11:11 +0200

    initial commit

---
+++ javascript/javascript2/week3/fastest_presser/fastest.js
@@ -0,0 +1,86 @@
+const btn = document.getElementById("btn");
+const winnerL = document.getElementById("winner_l");
+const winnerS = document.getElementById("winner_s");
+const counterL = document.getElementById("count_l");
+const counterS = document.getElementById("count_s");
+const timerDisplay = document.querySelector(".time_left");
+const reloadButton = document.getElementById("re_btn");
+const buttons = document.querySelectorAll("[data-time]");
+
+let time = 0;
+let clicked = false;
+
+function getTheTime(event) {
+  event.preventDefault();
+  event.target.setAttribute("disabled", true);
+  time = document.getElementById("time").value;
+  if (time === "") {
+    timerDisplay.innerHTML = "no time specified";
+  } else {
+    clicked = true;
+    timerDisplay.innerHTML = time; 
+  }
+  setCounter(time);
+}
+btn.addEventListener("click", getTheTime);
+
+function setCounter(time) {
+  setInterval(() => {
+    time--;
+    if ((time > 0)) {
+      timerDisplay.innerHTML = time;
+    } else if(time === 0){
+      timerDisplay.innerHTML = "End of the game"; 
+    }
+  }, 1000);
+}
+
+reloadButton.addEventListener("click", function() {
+  document.location.reload(true);
+});
+
+const countPress = event => {
+  const keyValue = event.key;
+  if (clicked === true && keyValue === "l") {
+    countL = parseInt(counterL.textContent || 0);
+    counterL.textContent = countL + 1;
+  } else if (clicked === true && keyValue === "s") {
+    countS = parseInt(counterS.textContent || 0);
+    counterS.textContent = countS + 1;
+  }
+  return false;
+};
+document.addEventListener("keyup", countPress);
+
+const getTheWinner = () => {
+  if (counterS.innerHTML === "" && counterL.innerHTML === "" && time === "") {
+    winnerL.textContent = "";
+    winnerS.textContent = "";
+  } else if (counterS.innerHTML === "" && counterL.innerHTML === "") {
+    winnerL.textContent = "You forgot to play";
+    winnerS.textContent = "You forgot to play";
+  } else if (counterS.innerHTML > counterL.innerHTML) {
+    winnerS.textContent = "You won!";
+  } else if (counterS.innerHTML < counterL.innerHTML) {
+    winnerL.textContent = "You won!";
+  } else if (counterS.innerHTML === counterL.innerHTML) {
+    winnerL.textContent = "You are both equaly good";
+    winnerS.textContent = "You are both equaly good";
+  }
+};
+const timeOut = () => setTimeout(getTheWinner, time * 1000);
+btn.addEventListener("click", timeOut);
+
+//I tried this optional part but it is not working correctly I will try to finish it during week 
+//my problem is with getTheWinner function which is invoked immediatelly or never 
+//also I would like to implement clearTimeout() function
+function startButtonTimer() {
+  clicked = true;
+  winnerL.textContent = "";
+  winnerS.textContent = "";
+  const time = (parseInt(this.dataset.time));
+  timerDisplay.innerHTML = time; 
+  setCounter(time);
+}
+
+buttons.forEach(button => button.addEventListener("click", startButtonTimer));
