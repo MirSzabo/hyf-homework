338caa93fdc3d009169729d581067c605127d253
MirSzabo <poradova.m@gmail.com>
2019-10-27 22:13:39 +0100

    add code for movies exercise

---
+++ javascript/javascript3/week2/movies/movies.js
@@ -0,0 +1,12 @@
+fetch(
+  "https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json"
+)
+  .then(response => response.json())
+  .then(data => {
+    const badMovies = data.filter(data => data.rating < 4);
+    console.log(badMovies);
+    const badMoviesSince2000 = badMovies.filter(data => data.year > 2000);
+    console.log(badMoviesSince2000);
+    const badMoviesSince2000Titles = badMoviesSince2000.map(data => data.title);
+    console.log(badMoviesSince2000Titles);
+  });
