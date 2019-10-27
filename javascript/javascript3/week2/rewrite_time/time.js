4e1ddea1109af091c2f419d8b7f799ba0fabb903
MirSzabo <poradova.m@gmail.com>
2019-10-27 22:27:24 +0100

    add code for Rewrite time exercise

---
+++ javascript/javascript3/week2/rewrite_time/time.js
@@ -0,0 +1,24 @@
+function setTimeoutPromise(time) {
+  return new Promise(resolve => {
+    setTimeout(resolve, time);
+  });
+}
+setTimeoutPromise(3000).then(() => {
+  console.log("Called after 3 seconds");
+});
+
+
+function getCurrentLocation() {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    })
+}
+getCurrentLocation()
+    .then((position) => {
+        // called when the users position is found
+        console.log(position);
+    })
+    .catch((error) => {
+        // called if there was an error getting the users location
+        console.log(error);
+    });
