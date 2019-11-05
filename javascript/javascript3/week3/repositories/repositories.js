7b29ab1fcf6a74bee15143741dd9a79a128bd982
MirSzabo <poradova.m@gmail.com>
2019-11-05 13:18:38 +0100

    catch error added

--- javascript/javascript3/week3/repositories/repositories.js
+++ javascript/javascript3/week3/repositories/repositories.js
@@ -9,23 +9,27 @@ const user3 = fetch(
 ).then(response => response.json());
 
 function getAllRepositories() {
-  Promise.all([user1, user2, user3]).then(repositoriesInfo => {
-    const newUl = document.createElement("ul");
-    document.body.appendChild(newUl);
-    for (let i = 0; i < repositoriesInfo.length; i++) {
-      const newLi = document.createElement("li");
-      newLi.innerHTML = repositoriesInfo[i].items[i].owner.login;
-      newUl.appendChild(newLi);
-      const newUlNested = document.createElement("ul");
-      newLi.appendChild(newUlNested);
-      for (let j = 0; j < repositoriesInfo[i].items.length; j++) {
-        const newLiNested = document.createElement("li");
-        newLiNested.innerHTML = `${repositoriesInfo[i].items[j].name}: ${
-          repositoriesInfo[i].items[j]["html_url"]
-        }`;
-        newUlNested.appendChild(newLiNested);
+  Promise.all([user1, user2, user3])
+    .then(repositoriesInfo => {
+      const newUl = document.createElement("ul");
+      document.body.appendChild(newUl);
+      for (let i = 0; i < repositoriesInfo.length; i++) {
+        const newLi = document.createElement("li");
+        newLi.innerHTML = repositoriesInfo[i].items[i].owner.login;
+        newUl.appendChild(newLi);
+        const newUlNested = document.createElement("ul");
+        newLi.appendChild(newUlNested);
+        for (let j = 0; j < repositoriesInfo[i].items.length; j++) {
+          const newLiNested = document.createElement("li");
+          newLiNested.innerHTML = `${repositoriesInfo[i].items[j].name}: ${
+            repositoriesInfo[i].items[j]["html_url"]
+          }`;
+          newUlNested.appendChild(newLiNested);
+        }
       }
-    }
-  });
+    })
+    .catch(error => {
+      console.log(error);
+    });
 }
 getAllRepositories();
