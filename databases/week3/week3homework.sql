4b49cde2733664266e529401b3ec7e76bd4e9668
MirSzabo <48127476+MirSzabo@users.noreply.github.com>
2019-12-02 23:37:30 +0100

    homework corrected

--- databases/week3/week3homework.sql
+++ databases/week3/week3homework.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE meal_sharing;
 USE meal_sharing;
-CREATE TABLE `Meal` (
+CREATE TABLE `meal` (
 	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
 	`title` varchar(255) NOT NULL,
 	`description` text NULL DEFAULT NULL,
@@ -11,7 +11,7 @@ CREATE TABLE `Meal` (
 	`created_date` DATETIME NOT NULL DEFAULT NOW(),
 PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-CREATE TABLE `Reservation` (
+CREATE TABLE `reservation` (
 	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
 	`number_of_guests` int(10) unsigned NOT NULL,
 	`meal_id` int(10) unsigned NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE `Reservation` (
     PRIMARY KEY (`id`),
 FOREIGN KEY (`meal_id`) REFERENCES `meal` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-CREATE TABLE `Review` (
+CREATE TABLE `review` (
 	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
 	`title` varchar(255) NOT NULL,
 	`description` text NULL DEFAULT NULL,
@@ -31,7 +31,7 @@ FOREIGN KEY (`meal_id`) REFERENCES `meal` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Get all meals
-SELECT * FROM Meal;
+SELECT * FROM meal;
 -- Add a new meal
 INSERT INTO `meal` (
     `title`,
@@ -85,18 +85,18 @@ INSERT INTO `meal` (
 	'2019-12-27 10:30:00'
 );
 -- Get a meal with any id, fx 1
-SELECT * FROM Meal
+SELECT * FROM meal
 WHERE `id` = 1;
 -- Update a meal with any id, fx 1. Update any attribute fx the title or multiple attributes
-UPDATE Meal 
+UPDATE meal 
 SET title = 'Pizza Haway' 
 WHERE id = 1;
 -- Delete a meal with any id, fx 1
-DELETE FROM Meal 
+DELETE FROM meal 
 WHERE id = 1;
 
 -- Get all reservations
-SELECT * FROM Reservation;
+SELECT * FROM reservation;
 -- Add a new reservation
 INSERT INTO `reservation` (
     `number_of_guests`,
@@ -126,18 +126,18 @@ INSERT INTO `reservation` (
 	'2019-11-26 08:15:00'
 );
 -- Get a reservation with any id, fx 1
-SELECT * FROM Reservation
+SELECT * FROM reservation
 WHERE `id` = 2;
 -- Update a reservation with any id, fx 1. Update any attribute fx the title or multiple attributes
-UPDATE Reservation 
+UPDATE reservation 
 SET number_of_guests = 1
 WHERE id = 2;
 -- Delete a reservation with any id, fx 1
-DELETE FROM Reservation 
+DELETE FROM reservation 
 WHERE id = 1;
 
 -- Get all reviews
-SELECT * FROM Review;
+SELECT * FROM review;
 -- Add a new review
 INSERT INTO `review` (
     `title`,
@@ -179,45 +179,45 @@ INSERT INTO `review` (
     DATE(NOW())
 );
 -- Get a review with any id, fx 1
-SELECT * FROM Review
+SELECT * FROM review
 WHERE id = 1;
 -- Update a review with any id, fx 1. Update any attribute fx the title or multiple attributes
-UPDATE Review
+UPDATE review
 SET title = 'very good experience'
 WHERE id = 1;
 -- Delete a review with any id, fx 1
-DELETE FROM Review 
+DELETE FROM review 
 WHERE id = 1;
 
 -- Get meals that has a price smaller than a specific price fx 90
-SELECT * FROM Meal
+SELECT * FROM meal
 WHERE meal.price < 90.00;
 -- Get meals that still has available reservations
 SELECT meal.id, meal.title, (meal.max_reservations-reservation.number_of_guests) AS `available_reservations`, reservation.number_of_guests FROM Meal
-INNER JOIN Reservation
+INNER JOIN reservation
 ON meal.id = reservation.meal_id
 WHERE number_of_guests < max_reservations;
 -- Get meals that partially match a title. Rød grød med will match the meal with the title Rød grød med fløde
-SELECT * FROM Meal
+SELECT * FROM meal
 WHERE title LIKE "%pizza v%";
 -- Get meals that has been created between two dates
-SELECT * FROM Meal
+SELECT * FROM meal
 WHERE `created_date` >= '2019-12-20' AND `created_date` <= '2019-12-28';
 -- Get only specific number of meals fx return only 5 meals
-SELECT * FROM Meal
+SELECT * FROM meal
 LIMIT 5;
 -- Get the meals that have good reviews
-SELECT meal.id, meal.title, meal.price, review.stars FROM Meal
-INNER JOIN Review
+SELECT meal.id, meal.title, meal.price, review.stars FROM meal
+INNER JOIN review
 ON meal.id = review.meal_id
 WHERE review.stars > 3;
 -- Get reservations for a specific meal sorted by created_date
-SELECT * FROM Reservation
+SELECT * FROM reservation
 WHERE `meal_id` = 2
 ORDER BY `created_date` DESC;
 -- Sort all meals by average number of stars in the reviews
-SELECT meal.title, AVG(review.stars) AS `average_rating` FROM Meal
-INNER JOIN Review
+SELECT meal.title, AVG(review.stars) AS `average_rating` FROM meal
+INNER JOIN review
 ON meal.id = review.meal_id
-GROUP BY meal.title
+GROUP BY meal.id
 ORDER BY review.stars DESC;
