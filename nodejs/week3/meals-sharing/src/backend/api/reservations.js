9a97e490826000c876028bcb3a6f3fdb174014ae
MirSzabo <48127476+MirSzabo@users.noreply.github.com>
2019-12-21 23:03:51 +0100

    reservations updated with routers

--- nodejs/week3/meals-sharing/src/backend/api/reservations.js
+++ nodejs/week3/meals-sharing/src/backend/api/reservations.js
@@ -1,7 +1,29 @@
 const express = require("express");
-const app = express();
-const router = express.Router();
-const pool = require("./../database");
+const route = express.Router();
+const reservations = require("../data/reservations");
 
+// Respond with the json for all reservations
+route.get("/reservations", (req, res) => {
+  res.json(reservations);
+});
 
-module.exports = router;
+//Respond with the json for the reservation with the corresponding id
+route.get("/reservations/:id", (req, res) => {
+  const { id } = req.params;
+  const isFound = reservations.some(
+    reservation => reservation.id === parseInt(id)
+  );
+  const isNumber = Number.isInteger(parseInt(id));
+
+  if (isFound && isNumber) {
+    res.send(
+      reservations.filter(reservation => {
+        return reservation.id === parseInt(id);
+      })
+    );
+  } else {
+    res.status(400).json({ msg: `No reservation with id of ${id}` });
+  }
+});
+
+module.exports = route;
