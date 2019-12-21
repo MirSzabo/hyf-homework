196705ea097e8cc65e545d8f74edb1ef82522156
MirSzabo <48127476+MirSzabo@users.noreply.github.com>
2019-12-21 23:10:22 +0100

    reviews updated with routers

--- nodejs/week3/meals-sharing/src/backend/api/reviews.js
+++ nodejs/week3/meals-sharing/src/backend/api/reviews.js
@@ -1,8 +1,27 @@
 const express = require("express");
-const app = express();
-const router = express.Router();
-const pool = require("./../database");
+const route = express.Router();
+const reviews = require("../data/reviews");
 
+// Respond with the json for all reviews
+route.get("/reviews", (req, res) => {
+  res.json(reviews);
+});
 
+//Respond with the json for the review with the corresponding id
+route.get("/reviews/:id", (req, res) => {
+  const { id } = req.params;
+  const isFound = reviews.some(review => review.id === parseInt(id));
+  const isNumber = Number.isInteger(parseInt(id));
 
-module.exports = router;
+  if (isFound && isNumber) {
+    res.send(
+      reviews.filter(review => {
+        return review.id === parseInt(id);
+      })
+    );
+  } else {
+    res.status(400).json({ msg: `No review with id of ${id}` });
+  }
+});
+
+module.exports = route;
