097df34883df4f63a792a650fd630da31928d80e
MirSzabo <48127476+MirSzabo@users.noreply.github.com>
2020-02-17 23:43:38 +0100

    comments implemented

--- nodejs/week3/warmup/index.js
+++ nodejs/week3/warmup/index.js
@@ -10,84 +10,73 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //Calculator using :param
 //calculator/addition?firstParam=1&secondParam=2&secondParam=4
 app.get("/calculator/addition", function(req, res) {
-  let values = [];
-  const firstParam = values.push(req.param("firstParam"));
-  const secondParam = values.push(req.param("secondParam"));
-  const valuesInOneArray = values.reduce((sum, val) => sum.concat(val), []);
+  const numbers = req.query;
+  const valuesInOneArray = [].concat.apply([], Object.values(numbers));
   const valuesToNumbers = valuesInOneArray.map(Number);
+  const result = valuesToNumbers.reduce((first, second) => {
+    return first - second;
+  });
 
-  if (valuesToNumbers.includes(NaN)) {
-    res.send("Please enter only numbers");
-  } else {
-    const result = valuesToNumbers.reduce((first, second) => {
-      return first + second;
-    });
-    res.send(`The addition of the numbers ${valuesInOneArray} is ${result}`);
-  }
+  valuesToNumbers.includes(NaN)
+  ? res.send("Please enter only numbers")
+  : res.send(
+      `The addition of the numbers ${valuesInOneArray} is ${result}`
+    );
 });
 
 //calculator/subtraction?firstParam=1&secondParam=2&secondParam=4
 app.get("/calculator/subtraction", function(req, res) {
-  let values = [];
-  const firstParam = values.push(req.param("firstParam"));
-  const secondParam = values.push(req.param("secondParam"));
-  const valuesInOneArray = values.reduce((sum, val) => sum.concat(val), []);
+  const numbers = req.query;
+  const valuesInOneArray = [].concat.apply([], Object.values(numbers));
   const valuesToNumbers = valuesInOneArray.map(Number);
+  const result = valuesToNumbers.reduce((first, second) => {
+    return first - second;
+  });
 
-  if (valuesToNumbers.includes(NaN)) {
-    res.send("Please enter only numbers");
-  } else {
-    const result = valuesToNumbers.reduce((first, second) => {
-      return first - second;
-    });
-    res.send(`The subtraction of the numbers ${valuesInOneArray} is ${result}`);
-  }
+  valuesToNumbers.includes(NaN)
+    ? res.send("Please enter only numbers")
+    : res.send(
+        `The subtraction of the numbers ${valuesInOneArray} is ${result}`
+      );
 });
 
 //calculator/multiply?firstParam=1&secondParam=2&secondParam=4
 app.get("/calculator/multiply", function(req, res) {
-  let values = [];
-  const firstParam = values.push(req.param("firstParam"));
-  const secondParam = values.push(req.param("secondParam"));
-  const valuesInOneArray = values.reduce((sum, val) => sum.concat(val), []);
+  const numbers = req.query;
+  const valuesInOneArray = [].concat.apply([], Object.values(numbers));
   const valuesToNumbers = valuesInOneArray.map(Number);
+  const result = valuesToNumbers.reduce((first, second) => {
+    return first - second;
+  });
 
-  if (valuesToNumbers.includes(NaN)) {
-    res.send("Please enter only numbers");
-  } else {
-    const result = valuesToNumbers.reduce((first, second) => {
-      return first * second;
-    });
-    res.send(`The multiply of the numbers ${valuesInOneArray} is ${result}`);
-  }
+  valuesToNumbers.includes(NaN)
+  ? res.send("Please enter only numbers")
+  : res.send(
+      `The multiply of the numbers ${valuesInOneArray} is ${result}`
+    );
 });
 
 //calculator/division?firstParam=1&secondParam=2&secondParam=4
 app.get("/calculator/division", function(req, res) {
-  let values = [];
-  const firstParam = values.push(req.param("firstParam"));
-  const secondParam = values.push(req.param("secondParam"));
-  const valuesInOneArray = values.reduce((sum, val) => sum.concat(val), []);
+  const numbers = req.query;
+  const valuesInOneArray = [].concat.apply([], Object.values(numbers));
   const valuesToNumbers = valuesInOneArray.map(Number);
+  const result = valuesToNumbers.reduce((first, second) => {
+    return first - second;
+  });
 
-  if (valuesToNumbers.includes(NaN)) {
-    res.send("Please enter only numbers");
-  } else {
-    const result = valuesToNumbers.reduce((first, second) => {
-      return first / second;
-    });
-    res.send(`The division of the numbers ${valuesInOneArray} is ${result}`);
-  }
+  valuesToNumbers.includes(NaN)
+  ? res.send("Please enter only numbers")
+  : res.send(
+      `The division of the numbers ${valuesInOneArray} is ${result}`
+    );
 });
 
 //Calculator using req.body
 //calculator?firstParam=1&secondParam=2 with the key method in the req.body set to multiply should respond with 2
 app.post("/calculator", (req, res) => {
-  let values = [];
-  const firstParam = values.push(req.param("firstParam"));
-  const secondParam = values.push(req.param("secondParam"));
-  const operation = req.body.operation;
-  const valuesInOneArray = values.reduce((sum, val) => sum.concat(val), []);
+  const numbers = req.query;
+  const valuesInOneArray = [].concat.apply([], Object.values(numbers));
   const valuesToNumbers = valuesInOneArray.map(Number);
 
   if (valuesToNumbers.includes(NaN)) {
